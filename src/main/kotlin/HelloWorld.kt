7f1d419019ad65d607642a2d690d2cdefbe52412
Futa Hirakoba <korosuke613613@gmail.com>
2017-10-04 00:13:07 +0900

    init

---
+++ src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,3 @@
+fun main(args:Array<String>)=println("Hello, World!")
+
+fun return1()=1
---
+++ src/test/kotlin/Test.kt
@@ -0,0 +1,10 @@
+import org.junit.Test
+import org.junit.Assert
+
+class Test
+{
+    @Test fun testNum()
+    {
+        Assert.assertEquals(1, return1())
+    }
+}
